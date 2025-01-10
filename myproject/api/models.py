3a722eee7bc1b21ea1d27961f714ec3cee8605ec
Кабидоллаев Гашиых <gashiykh@bk.ru>
2025-01-10 17:40:09 +0500

    Добавил айди Продукта

--- myproject/api/models.py
+++ myproject/api/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, verbose_name="Название продукта")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Стоимость продукта")
 
     def __str__(self):
         return self.name
+    
+    
+
