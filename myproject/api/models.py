82e21ebf8d1a9bc563ec040316574f6f434e11f0
Кабидоллаев Гашиых <gashiykh@bk.ru>
2025-01-10 17:47:33 +0500

    Добавил модель Заказы

--- myproject/api/models.py
+++ myproject/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
@@ -8,5 +9,20 @@ class Product(models.Model):
     def __str__(self):
         return self.name
     
-    
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('confirmed', 'Confirmed'),
+        ('cancelled', 'Cancelled'),
+    ]
+
+    order_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(get_user_model, related_name="orders", on_delete=models.CASCADE, verbose_name="Клиент")
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending', verbose_name="Статус заказа")
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Общая стоимость")
+    products = models.ManyToManyField(Product, through='OrderProduct', related_name='orders', verbose_name="Продукты")
+
+    def __str__(self):
+        return f"Заказ {self.order_id} от {self.user.name}"
 
