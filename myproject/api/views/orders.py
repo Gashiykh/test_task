a9e0ad11827835be333696cad8ec316a722e273c
Кабидоллаев Гашиых <gashiykh@bk.ru>
2025-01-11 14:21:39 +0500

    ПОдставляет айди текущего пользователя

--- myproject/api/views/orders.py
+++ myproject/api/views/orders.py
@@ -62,7 +62,7 @@ class OrderView(APIView):
         )
 
         if serializer.is_valid():
-            serializer.save(user=request.user)
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
