6ef06c190e5118ea867aebfe2e11969fe98632ec
Кабидоллаев Гашиых <gashiykh@bk.ru>
2025-01-11 12:43:54 +0500

    Добавил модели в админку для отображения

--- myproject/api/admin.py
+++ myproject/api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from api.models import (
+    Product,
+    Order,
+    OrderProduct
+)
+from users.models import User
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderProduct)
