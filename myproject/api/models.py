87aa6c47a9c5b7f78a266c838b4906fb7a111478
Кабидоллаев Гашиых <gashiykh@bk.ru>
2025-01-11 12:15:48 +0500

    Добавил динамическое вычисление общей стоимости Заказа

--- myproject/api/models.py
+++ myproject/api/models.py
@@ -21,12 +21,20 @@ class Order(models.Model):
     order_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(get_user_model(), related_name="orders", on_delete=models.CASCADE, verbose_name="Клиент")
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending', verbose_name="Статус заказа")
-    total_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Общая стоимость")
     products = models.ManyToManyField(Product, through='OrderProduct', related_name='orders', verbose_name="Продукты")
     is_deleted = models.BooleanField(default=False, verbose_name="Удалён")
 
+    def total_price(self):
+        total = 0
+        for item in self.order_product.all():
+            price = item.product.price
+            quantity = item.quantity
+            item_total = price * quantity
+            total += item_total
+        return round(total, 2)
+
     def __str__(self):
-        return f"Заказ {self.order_id} от {self.user.name}"
+        return f"Заказ {self.order_id} от {self.user.username}"
 
 
 class OrderProduct(models.Model):
