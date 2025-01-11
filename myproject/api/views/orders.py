750abac1da7986e57bb79e889623124e901e5160
Кабидоллаев Гашиых <gashiykh@bk.ru>
2025-01-11 12:52:15 +0500

    Добавил метод для добавления Заказа

--- myproject/api/views/orders.py
+++ myproject/api/views/orders.py
@@ -54,11 +54,14 @@ class OrderView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
     
     def post(self, request):
-        serializer = OrderSerializer(data=request.data)
+
+        serializer = OrderSerializer(
+            data=request.data,
+            context={'request': request}
+        )
 
         if serializer.is_valid():
-            serializer.save()
-
+            serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
