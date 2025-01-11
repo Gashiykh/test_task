253b13c66843de8496ffc46c2c898ddf74f8427a
Кабидоллаев Гашиых <gashiykh@bk.ru>
2025-01-11 15:46:56 +0500

    Добавил кастомную авторизацяю для отслеживания авторизации пользователя

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
+import logging
 
-# Create your views here.
+logger = logging.getLogger('user_actions')
+
+class CustomAuth(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        token = response.data.get('token')
+        if token:
+            logger.info(f"Пользователь {request.user.username} авторизован")
+        else:
+            logger.warning(f"Неудачная авторизация для пользователя {request.data.get('username')}.")
+        return response
